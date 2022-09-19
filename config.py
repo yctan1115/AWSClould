a3d2cd701e7e7cb1b392f84892abb05192241e8b
yctan <69790163+yctan1115@users.noreply.github.com>
2022-09-19 13:34:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ custompass = "Bait3273"
 customdb = "employee"
 custombucket = "lowchoonkeat-employee"
 customregion = "us-east-1"
+aws_access_key_id=ASIAVC3M4HO65CC4ENY3
+aws_secret_access_key=GCSYKKE3B49Dvqt4rpsfOpKouraTed3sCALsbAlK
+aws_session_token=FwoGZXIvYXdzEKf//////////wEaDKRMEefNnzMDcN4zQCLPAcUgcIOMqAdvXyHR6kE0mici8LvV8PBLV/OAm66GlIkzsiieB7KBsoE3PcBAyJhAL82btL/xor0saGmA0sujIOUSSrtCQCPqg2YInp4Z2bULOOb2wZXjNXlsoM9B8YhT5XluuD57nTzz6glHJ1oRHxz8Yr5zgb4BuPIZiNksEaLdVC24F8iHb69pR8CzIeqNzqGoWhh1pAktlOPa2t/3AxITRsKob9Y+lfC3MqOHlaw5zNj2r1FptLxl2yGwZey5TRRSAumdrAeCORBNrHDoDSjO+Z+ZBjIt0T7x7Mwx5jG+yXE9Gk27FtYhcut26gJs7tyf/GKNW02nvprtqqtmEEq9zszp
